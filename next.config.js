5a16c7bf744bbef59993f9e28364f822e239c052
Robert Koch <robert@kochie.io>
2019-05-03 19:41:10 +0800

    added now sw config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,27 @@ const withCss = require('@zeit/next-css')
 const withOffline = require('next-offline')
 
 const config = {
-    target: "serverless"
+    target: "serverless",
+    workboxOpts: {
+        swDest: 'static/service-worker.js',
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: 'networkFirst',
+            options: {
+              cacheName: 'https-calls',
+              networkTimeoutSeconds: 15,
+              expiration: {
+                maxEntries: 150,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+    }
 }
 
 module.exports = withOffline(withCss(withMDX(withTypescript(config))))
