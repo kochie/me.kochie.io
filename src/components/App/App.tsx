edb2b8d0ee8d2288c0ec8adc92bf2c857d9a9c6c
Robert Koch <robert@kochie.io>
2019-05-03 20:25:29 +0800

    Added web manifest to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,7 +16,7 @@ export default class App extends React.Component {
       <div className="app">
         <Head>
           <title>Robert Koch</title>
-
+          <link rel="manifest" href="/manifest.webmanifest" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="UTF-8" />
           <meta
