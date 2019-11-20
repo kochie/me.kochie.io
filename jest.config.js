529c19eced7f115442061a60c663ef42122c2654
Robert Koch <robert@kochie.io>
2019-11-21 00:41:25 +1100

    Updated snapshots

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
+
 module.exports = {
-  preset: 'ts-jest',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testRegex: TEST_REGEX,
   transform: {
+    '^.+\\.tsx?$': 'babel-jest',
     "^.+\\.mdx?$": "<rootDir>/mdx-jest.js"
-  }
-};
+  },
+  moduleNameMapper: {
+    "^.+\\.css?$": "<rootDir>/node_modules/jest-css-modules"
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverage: false
+}
--- src/components/ContactButton/__tests__/ContactButton.test.tsx
+++ src/components/ContactButton/__tests__/ContactButton.test.tsx
@@ -6,7 +6,7 @@ import ContactButton from "../ContactButton";
 it("contact button renders correctly", () => {
   const tree = renderer
     .create(
-      <ContactButton name="test" email="test@test.com" message="a message" />
+      <ContactButton name="test" message="a message" />
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
