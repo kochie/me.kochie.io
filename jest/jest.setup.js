36a3e44dc5cbd81fe7d199bb640a79f0f44049a8
Robert Koch <robert@kochie.io>
2020-10-31 23:22:10 +1100

    added new Image component

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -2,3 +2,14 @@ const Enzyme = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
 
 Enzyme.configure({ adapter: new Adapter() })
+
+process.env = {
+    ...process.env,
+    __NEXT_IMAGE_OPTS: {
+        deviceSizes: [320, 420, 768, 1024, 1200],
+        imageSizes: [],
+        domains: ['me.kochie.io'],
+        path: '/_next/image',
+        loader: 'default',
+    },
+};
