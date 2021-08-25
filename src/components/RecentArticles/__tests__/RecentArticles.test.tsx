261b63407557f2dae5fc55afd6219052129224c6
Robert Koch <robert@kochie.io>
2021-08-26 01:58:28 +1000

    got swr mock working

--- src/components/RecentArticles/__tests__/RecentArticles.test.tsx
+++ src/components/RecentArticles/__tests__/RecentArticles.test.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 import RecentArticles from '../RecentArticles'
 
-it('recent articles renders correctly', () => {
-  const tree = renderer.create(<RecentArticles />).toJSON()
-  expect(tree).toMatchSnapshot()
+it('recent articles renders correctly', async () => {
+  const tree = renderer.create(<RecentArticles />)
+  expect(tree.toJSON()).toMatchSnapshot()
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import React, { ReactElement } from 'react'
+import Script from 'next/script'
 
-import { Head, App, BuyMeACoffee } from '@/components/index'
+import { Head, App } from '@/components/index'
 
 const Index = (): ReactElement => (
   <>
     <Head />
     <App />
-    <BuyMeACoffee />
+    <Script
+      data-name="BMC-Widget"
+      data-cfasync="false"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+      data-id="kochie"
+      data-description="Support me on Buy me a coffee!"
+      data-message=""
+      data-color="#FF5F5F"
+      data-position="Right"
+      data-x_margin="18"
+      data-y_margin="18"
+      strategy="lazyOnload"
+    />
   </>
 )
 
