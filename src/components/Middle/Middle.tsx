f207e99e19d8a0d87d7f16a40ecf3699244c9607
Robert Koch <robert@kochie.io>
2021-09-16 12:18:34 +1000

    Dev/reduce container size (#315)
    * bumped dependencies and updated tests
    
    * reduced the max size of the container

--- src/components/Middle/Middle.tsx
+++ src/components/Middle/Middle.tsx
@@ -9,33 +9,35 @@ import GetInTouch from '@/components/GetInTouch'
 export default function Middle(): ReactElement {
   return (
     <div className="p-2 pt-6 lg:p-24 inline-grid bg-gray-200 dark:bg-gray-800 transform-gpu transition duration-700">
-      <MDXProvider
-        components={{
-          h1: ({ children }) => (
-            <h1 className="text-gray-900 text-4xl font-extrabold mb-3.5 dark:text-white">
-              {children}
-            </h1>
-          ),
-          p: ({ children }) => (
-            <p className="text-gray-700 my-3 leading-relaxed dark:text-gray-400">
-              {children}
-            </p>
-          ),
-          a: ({ children, href }) => (
-            <a
-              href={href}
-              className="underline dark:text-orange-300 dark:hover:text-orange-600 text-indigo-600 hover:text-indigo-900 cursor-pointer"
-            >
-              {children}
-            </a>
-          ),
-        }}
-      >
-        <Hello />
-      </MDXProvider>
-      <GetInTouch />
-      <Portfolio />
-      <RecentArticles />
+      <div className="max-w-7xl">
+        <MDXProvider
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-gray-900 text-4xl font-extrabold mb-3.5 dark:text-white">
+                {children}
+              </h1>
+            ),
+            p: ({ children }) => (
+              <p className="text-gray-700 my-3 leading-relaxed dark:text-gray-400">
+                {children}
+              </p>
+            ),
+            a: ({ children, href }) => (
+              <a
+                href={href}
+                className="underline dark:text-orange-300 dark:hover:text-orange-600 text-indigo-600 hover:text-indigo-900 cursor-pointer"
+              >
+                {children}
+              </a>
+            ),
+          }}
+        >
+          <Hello />
+        </MDXProvider>
+        <GetInTouch />
+        <Portfolio />
+        <RecentArticles />
+      </div>
     </div>
   )
 }
