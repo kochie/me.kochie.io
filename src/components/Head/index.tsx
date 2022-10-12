7806095b222341bce3ea84eb0ad6d2580030d3d9
Robert Koch <robert@kochie.io>
2022-10-13 01:40:55 +1100

    added icons

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -4,34 +4,15 @@ import Head from 'next/head'
 export default function Heading(): ReactElement {
   return (
     <Head>
-      <meta name="application-name" content="PWA App" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="PWA App" />
-      <meta name="description" content="Best PWA App in the world" />
-      <meta name="format-detection" content="telephone=no" />
+      <meta name="apple-mobile-web-app-title" content="Robert Koch" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-      <meta name="msapplication-TileColor" content="#2B5797" />
+      <meta name="msapplication-TileColor" content="#1f2937" />
       <meta name="msapplication-tap-highlight" content="no" />
-      <meta name="theme-color" content="#000000" />
+      <meta name="theme-color" content="#1f2937" />
 
-      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/icons/touch-icon-ipad.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/icons/touch-icon-iphone-retina.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="167x167"
-        href="/icons/touch-icon-ipad-retina.png"
-      />
+      <link rel="manifest" href="/manifest.json" />
 
       <link
         rel="icon"
@@ -46,15 +27,9 @@ export default function Heading(): ReactElement {
         href="/icons/favicon-16x16.png"
       />
       <link rel="manifest" href="/manifest.json" />
-      <link
-        rel="mask-icon"
-        href="/icons/safari-pinned-tab.svg"
-        color="#5bbad5"
-      />
+      <link rel="mask-icon" href="/images/favicon_curved.svg" color="#1f2937" />
 
       <link rel="apple-touch-icon" href="/assets/apple-icon-180.png" />
-
-      <meta name="apple-mobile-web-app-capable" content="yes" />
 
       <link
         rel="apple-touch-startup-image"
