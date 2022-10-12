2b8ae2ae4e39d7c2c20a5749540bd2ff2c0e8b3f
Robert Koch <robert@kochie.io>
2022-10-12 14:04:55 +1100

    refactor and colour change

--- src/components/Awards/index.tsx
+++ src/components/Awards/index.tsx
@@ -2,32 +2,34 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { forwardRef } from 'react'
 
-const Img = (props, ref) => (
-  <div ref={ref}>
+const HolopinImage = ({ user }, ref) => (
+  <a ref={ref}>
     <Image
-      className="rounded-xl cursor-pointer"
-      src="https://holopin.io/api/user/board?user=kochie"
-      alt="@kochie's Holopin board"
+      className="rounded-xl cursor-pointer grayscale-50 hover:grayscale-0 transform-gpu duration-200"
+      src={`https://holopin.io/api/user/board?user=${user}`}
+      alt={`@${user}'s Holopin board`}
       width={2428}
       height={764}
     />
-  </div>
+  </a>
 )
 
-const ImageRef = forwardRef(Img)
+const HolopinRef = forwardRef(HolopinImage)
+
+const Holopin = ({ user }) => (
+  <div className="w-full">
+    <Link href={`https://holopin.io/@${user}`}>
+      <HolopinRef user={user} />
+    </Link>
+  </div>
+)
 
 const Awards = () => (
   <div>
     <h2 className="text-gray-900 dark:text-white font-bold text-2xl mt-6 mb-4">
       Awards
     </h2>
-    <div className="w-full">
-      <Link href="https://holopin.io/@kochie">
-        <a>
-          <ImageRef />
-        </a>
-      </Link>
-    </div>
+    <Holopin user="kochie" />
   </div>
 )
 
