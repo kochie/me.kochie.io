d41c4517471fe0998df3eef4e2ce204fa84879cc
Robert Koch <robert@kochie.io>
2023-05-08 11:29:29 +1000

    bumps packages

--- src/app/api/holopin/route.ts
+++ src/app/api/holopin/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export const config = {
-  runtime: 'edge',
-}
+export const runtime = 'edge'
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
